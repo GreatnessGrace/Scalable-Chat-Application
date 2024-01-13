b552257939441405c3854e391c37c4d5e2ce218f
GreatnessGrace <graceunstoppable7@gmail.com>
2024-01-14 00:04:18 +0530

    [Completed] Socket.io Setup

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,26 @@
+import { Server } from 'socket.io'
+
+class SocketService {
+    private _io: Server;  // instance variable of our class
+    constructor() {
+        console.log("Init Socket Service...")
+        this._io = new Server() // initialising socket.io server
+    }
+
+    public initListeners() {
+        const io = this.io;
+        io.on('connect', (socket) => {
+            console.log(`new socket connected`, socket.id);
+
+            socket.on('event:message', async ({ message }: { message: string }) => {
+                console.log('New Message Rec', message)
+            })
+        })
+    }
+
+    get io() {
+        return this._io;
+    }
+}
+
+export default SocketService;
