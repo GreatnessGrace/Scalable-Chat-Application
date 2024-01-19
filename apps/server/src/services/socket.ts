3a8e5f84a4182db4a3a03dbd6d41279cbc17933c
GreatnessGrace <graceunstoppabel7@gmail.com>
2024-01-19 12:26:02 +0530

    [Completed] Connected  Redis

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,16 +1,32 @@
 import { Server } from 'socket.io'
+import Redis from 'ioredis'
+const pub = new Redis({
+  host: "redis-2f076575-graceunstoppable7-e120.a.aivencloud.com",
+  port: 13236,
+  username: "default",
+  password: "AVNS_4WuXBm75Dm7V-yrpfVy",
+});
+
+const sub = new Redis({
+  host: "redis-2f076575-graceunstoppable7-e120.a.aivencloud.com",
+  port: 13236,
+  username: "default",
+  password: "AVNS_4WuXBm75Dm7V-yrpfVy",
+});
 
 class SocketService {
     private _io: Server;  // instance variable of our class
+
     constructor() {
         console.log("Init Socket Service...");
         this._io = new Server({
           cors: {
             allowedHeaders: ["*"],
-            methods: ["GET", "POST"],
+            // methods: ["GET", "POST"],
             origin: "*",
           },
         });
+        sub.subscribe("MESSAGES");
       }
 
     public initListeners() {
@@ -21,8 +37,17 @@ class SocketService {
             socket.on('event:message', async ({ message }: { message: string }) => {
                 console.log('New Message Received', message)
                 // Publish this message to redis
+                await pub.publish("MESSAGES", JSON.stringify({ message }));
             })
         })
+        sub.on("message", async (channel, message) => {
+          if (channel === "MESSAGES") {
+            console.log("new message from redis", message);
+            io.emit("message", message);
+            // await produceMessage(message);
+            // console.log("Message Produced to Kafka Broker");
+          }
+        });
     }
 
     get io() {
