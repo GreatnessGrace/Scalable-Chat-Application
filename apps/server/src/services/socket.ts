8d1b2ad9d7a972e6aef368b7390d2ad82252f651
GreatnessGrace <graceunstoppabel7@gmail.com>
2024-01-19 14:13:26 +0530

    Credentials Protection

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,17 +1,27 @@
 import { Server } from 'socket.io'
 import Redis from 'ioredis'
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const Host = process.env.HOST;
+const Port = process.env.PORT ? parseInt(process.env.PORT, 10) : undefined; // Convert PORT to a number
+const Username = process.env.USERNAME;
+const Password = process.env.PASSWORD;
+console.log("----------------------",Host, Port, Username, Password);
+
+// import config from 'process.env'
 const pub = new Redis({
-  host: "redis-2f076575-graceunstoppable7-e120.a.aivencloud.com",
-  port: 13236,
-  username: "default",
-  password: "AVNS_4WuXBm75Dm7V-yrpfVy",
+  host: Host,
+  port: Port,
+  username: Username,
+  password: Password,
 });
 
 const sub = new Redis({
-  host: "redis-2f076575-graceunstoppable7-e120.a.aivencloud.com",
-  port: 13236,
-  username: "default",
-  password: "AVNS_4WuXBm75Dm7V-yrpfVy",
+  host: Host,
+  port: Port,
+  username: Username,
+  password: Password,
 });
 
 class SocketService {
